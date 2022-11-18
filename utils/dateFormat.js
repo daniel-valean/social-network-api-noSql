2448954e435b4df11128be20c1d9c7d643506e8b
daniel-valean <dannyvalean@gmail.com>
2022-11-17 23:59:40 -0700

    fixed bug

--- utils/dateFormat.js
+++ utils/dateFormat.js
@@ -53,3 +53,41 @@ module.exports = (
             11: 'December'
         };
     }
+
+    const dateObj = new Date(timestamp);
+    const formattedMonth = months[dateObj.getMonth()];
+
+    let dayOfMonth;
+
+    if (dateSuffix) {
+        dayOfMonth = addDateSuffix(dateObj.getDate());
+    } else {
+        dayOfMonth = dateObj.getDate();
+    }
+
+    const year = dateObj.getFullYear();
+
+    let hour;
+    if (dateObj.getHours > 12) {
+        hour = Math.floor(dateObj.getHours() / 2);
+    } else {
+        hour = dateObj.getHours();
+    }
+    if (hour === 0) {
+        hour = 12;
+    }
+
+    const minutes = dateObj.getMinutes();
+
+    let periodOfDay;
+
+    if (dateObj.getHours() >= 12) {
+        periodOfDay = 'pm';
+    } else {
+        periodOfDay = 'am';
+    }
+
+    const formattedTimeStamp = `${formattedMonth} ${dayOfMonth}, ${year} at ${hour}:${minutes} ${periodOfDay}`;
+
+    return formattedTimeStamp;
+};2
